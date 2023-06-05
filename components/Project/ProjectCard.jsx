823ebe9baa62218af659d34d03fd46e811e6a1e3
notVinh <dqv4work@gmail.com>
2023-06-05 15:30:33 +0700

    final respons in mobile

--- components/Project/ProjectCard.jsx
+++ components/Project/ProjectCard.jsx
@@ -27,27 +27,27 @@ const ProjectCard = ({ order, img, name, desc, usage, codeLink, demoLink }) => {
         </p>
         <div className="flex justify-center items-center pb-9">
           {usage.map((item) => (
-            <div className="flex font-bold text-1xl xl:mx-5">{item}</div>
+            <div className="flex font-bold text-1xl xl:mx-5 mx-3">{item}</div>
           ))}
         </div>
         <div className="flex justify-center items-center">
           <div className="flex font-medium text-1xl">
             <a
               href={codeLink}
-              className="flex justify-center items-center xl:mx-5"
+              className="flex justify-center items-center xl:mx-5 mx-3"
             >
               Code
-              <span className="pl-2">
+              <span className="p-2">
                 <BsGithub />
               </span>
             </a>
             <a
               href={demoLink}
-              className="flex justify-center items-center mx-5"
+              className="flex justify-center items-center xl:mx-5 mx-3"
               target="_blank"
             >
               Demo
-              <span className="pl-2">
+              <span className="p-2">
                 <IoOpenOutline />
               </span>
             </a>
